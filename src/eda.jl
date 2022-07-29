9a5ea1076ac64ec2c5b92366d052885c65886707
indymnv <indy.mnv@gmail.com>
2022-07-29 19:41:36 +0900

    inserting first plot

--- src/eda.jl
+++ src/eda.jl
@@ -14,10 +14,26 @@ function read_df()
 
 end
 
+# Create DataFrames
 train, test, sub, labels = read_df()
 
+#Transform string to Dates
 train.S_2 = Date.(train.S_2, "yyyy-mm-dd")
+test.S_2 = Date.(test.S_2, "yyyy-mm-dd")
 
-customer = train.customer_ID[1]
+#Select first customer in train DataFrame
 
+function filter_by_client(position::Int)
+  customer = train.customer_ID[position]
+  first_client(name::String)= name == customer #Create function to filter the client
+  client_df = filter(:customer_ID => first_client, train)
+  
+  return client_df
 
+end
+
+client_df = filter_by_client(56)
+
+plot( client_df.S_2, client_df.P_2 )
+plot!( client_df.S_2, client_df.B_2 )
+plot!(title="P_2 for client A")
